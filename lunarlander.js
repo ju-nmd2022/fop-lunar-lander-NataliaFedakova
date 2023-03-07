c07158ed0f7089e8b10e52461fccf8fd92035763
NataliaFedakova <117270929+NataliaFedakova@users.noreply.github.com>
2023-03-07 18:43:52 +0100

    Update lunarlander.js

--- lunarlander.js
+++ lunarlander.js
@@ -1,7 +1,7 @@
 function setup(){
   createCanvas(600, 700);
   button = createButton("PLAY AGAIN");
-  button.position(225, 250); 
+  button.position(425, 250); 
   button.size(200, 50);
   button.hide();
   button.mousePressed(playAgain);  
