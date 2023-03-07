66240457b4ac2c19a44a1bc15126c48afad85bdc
NataliaFedakova <117270929+NataliaFedakova@users.noreply.github.com>
2023-03-07 19:21:13 +0100

    Update lunarlander.js

--- lunarlander.js
+++ lunarlander.js
@@ -192,7 +192,7 @@ function startScreen() {
     
     fill(255, 255, 255);   
     button.show();
-    button.position(685, 250);   
+    button.position(620, 250);   
     
     fill ("#700124"); 
     beginShape();
@@ -328,7 +328,7 @@ noStroke();
   textSize(35);
   text("YOU WON", 230, 380); 
   button.show();
-  button.position(680, 450); 
+  button.position(620, 450); 
   }
 
 let isGameActive = true;
